31a77cba068570726b6c58135ae28fa342875373
amy-vision <24asinghasingh@gmail.com>
2026-03-29 22:03:54 +0530

    Update transformer.py

--- Transformer(withoutdc)/transformer.py
+++ Transformer(withoutdc)/transformer.py
@@ -2,6 +2,8 @@ from transformers import BertConfig,BertModel
 import torch
 import torch.nn as nn
 import random
+import torch 
+import torch.optim as optim
 
 #building the tokenizer
 dict={'A':0,'T':1,'G':2,'C':3}
@@ -15,10 +17,10 @@ print(tokenize(seq))
 
 
 config=BertConfig(
-    vocab_size=4,
-    hidden_size=64,
-    num_hidden_layers=2,
-    num_attention_heads=2
+    vocab_size=4, #no of possible tokens
+    hidden_size=64, # size of internal vector 
+    num_hidden_layers=2, #no of transformer layers
+    num_attention_heads=2 #no of heads
 )
 model=BertModel(config)
 
@@ -29,12 +31,12 @@ output=model(input_ids=tokens)
 class DNAClassifier(nn.Module):
     def __init__(self,transformer):
         super().__init__()
-        self.transformer=transformer
-        self.classifier=nn.Linear(64,2)
+        self.transformer=transformer #bert transformer 
+        self.classifier=nn.Linear(64,2) # takes 64 numbers and give output as 2 numbers
 
     def forward(self,x):
             outputs=self.transformer(input_ids=x)
-            cls_token=outputs.last_hidden_state[:,0,:]
+            cls_token=outputs.last_hidden_state[:,0,:] #takes the first letter of all seq
             return self.classifier(cls_token)
         
 
@@ -48,24 +50,21 @@ def generate_data(n=100):
          y.append(label)
          return X,y
     
-import torch 
-import torch.optim as optim
 
 X,y=generate_data(200)
-
 X=torch.tensor(X)
 y=torch.tensor(y)
 
 model=DNAClassifier(model)
-optimizer=optim.Adam(model.parameters(),lr=0.001)
-loss_fn=nn.CrossEntropyLoss()
+optimizer=optim.Adam(model.parameters(),lr=0.001) #Adam algorithm to manage model's weight to reduce the error 
+loss_fn=nn.CrossEntropyLoss()#for calculating loss
 
 for epoch in range(5):
-     optimizer.zero_grad()
-     preds=model(X)
-     loss=loss_fn(preds,y)
-     loss.backward()
-     optimizer.step()
+     optimizer.zero_grad() #clears out memory of prev losses
+     preds=model(X)#looks at model and give its current best guess
+     loss=loss_fn(preds,y)#checks the loss
+     loss.backward()#goes backward to check which neuron caused the error
+     optimizer.step()#changes weight to make it more accurate
      print("Loss:",loss.item())
 
         
